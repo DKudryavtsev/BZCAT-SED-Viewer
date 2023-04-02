9b660d8d4405387574f6d7d2a0b7587fd19ba09a
DKudryavtsev <folderdk@gmail.com>
2023-04-02 18:16:06 +0300

    empty non-residents corrected, left-right check removed

--- README.md
+++ README.md
@@ -4,12 +4,12 @@
 
 ## Description
 
-The BZCAT SED Viewer script displays the spectral energy distributions (SEDs) for the blazars from the [Roma-BZCAT catalog](https://heasarc.gsfc.nasa.gov/W3Browse/all/romabzcat.html) (5th edition, Massaro et al., 2015, Ap&SS, 357, 75). It also calculates the frequency of the synchrotron component maximum with a third-degree polinomial and allows a user to interactively mark it as 'good' or 'bad'. The results are saved in the original file. One can use the total list of Roma-BZCAT objects (data/BZCAT.csv provided here) or make their own subsample (the file must contain the 'BZCAT5 Source name', 'RA (J2000.0)', and 'Dec (J2000.0)' columns).
+The BZCAT SED Viewer script displays the spectral energy distributions (SEDs) for the blazars from the [Roma-BZCAT catalog](https://heasarc.gsfc.nasa.gov/W3Browse/all/romabzcat.html) (5th edition, Massaro et al., 2015, Ap&SS, 357, 75). It also calculates the frequency of the synchrotron component maximum using a third-degree or 2-degree polinomial and allows a user to interactively mark it as 'good' or 'bad'. The results are saved in the original file. One can use the total list of Roma-BZCAT objects (data/BZCAT.csv provided here) or make their own subsample (the file must contain the 'BZCAT5 Source name', 'RA (J2000.0)', and 'Dec (J2000.0)' columns). 
 
 ## Data
 
 * [List of blazars](./data/BZCAT.csv) from the [Roma-BZCAT catalog](https://heasarc.gsfc.nasa.gov/W3Browse/all/romabzcat.html)
-* Spectral energy distributions from [SED Builder](https://tools.ssdc.asi.it/SED/)
+* Spectral energy distributions for them from [SED Builder](https://tools.ssdc.asi.it/SED/) as of mid-2022. 
 
 ## Dependencies
 
@@ -23,9 +23,9 @@ python3 sed_viewer.py data/BZCAT.csv 1
 
 Here
 * data/BZCAT.csv - the provided file with BZCAT objects and their coordinates (or a file with your subsample)
-* 1 - the number of the object your want to begin with (the first object in the list corresponds to 1)
+* 1 - the index number of the object your want to begin with (the first object in the list corresponds to 1, the range is 1-3561)
 
-The script makes two charts - for the data from the SED Builder resident catalogs and for all catalogs. The 3rd degree polynomial fits the resident catalogs only.
+The script makes two charts - for the data from the SED Builder resident catalogs and for all catalogs. The polynomial fits the resident catalogs only, you can choose the 3rd (best practice) or 2nd degree. The right part of the synchrotron radiation SED is often "contaminated" by the radiation of the host galaxy. For that reason we implemented the slider which allows to tune the higher (right) frequency bound for the polynomial.
 
 Hotkeys in the graphics window:
 * Enter or Space - see the next object;
@@ -36,4 +36,6 @@ Hotkeys in the graphics window:
 * 'f' - full screen on/off;
 * 'q' - quit
 
-The marking labels are saved in the original file along with the synchrotron peak frequency and are available during the following script runnings. One can also change and clean the labels.
+Objects labelled as "good" (correctness == True) will be blocked from further fitting! To continue the fitting, clear the mark first. 
+
+The marking labels are saved in the original file along with the synchrotron peak frequency, right frequency bound, and polynomial order and are available during the following script runnings. One can also change and clean the labels.
--- sed_viewer.py
+++ sed_viewer.py
@@ -1,56 +1,25 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
+from matplotlib.widgets import Slider, RadioButtons
 import sys
 import os
 import warnings
 from scipy.optimize import minimize_scalar
 
-
 PATH = os.path.dirname(os.path.realpath(__file__))
 FILE_SEDS_RES = os.path.join(PATH, 'data/seds_residents.csv')
 FILE_SEDS_ALL = os.path.join(PATH, 'data/seds_all.csv')
+LOW_BOUND = 10  # Left frequency bound (for the optimizer only)
 
 
-def fit_poly(df, n, seds_res, seds_all, high_bound=15.5):
-    """Fitting a polynomial to an object's SED
-       n is the row number
-    """
+def fit3deg(sed_pol, high_bound):
+    "Fitting a 3rd degree polynomial"
     def f(x):
         return -(w[0] + w[1]*x + w[2]*x**2 + w[3]*x**3)
-        
-    # Constraints
-    LOW_BOUND =10
-    LOW_HIGH_BORDER = 12
     
-    source = df.iloc[n-1]['BZCAT5 Source name']
-    goodbad = df.iloc[n-1]['Correct']
-    sed_res = seds_res[seds_res['Source'] == df.iloc[n-1]['Source']]
-    sed_all = seds_all[seds_all['Source'] == df.iloc[n-1]['Source']]
-    
-    # Sometimes coordinates don't match in Dec arcseconds
-    if len(sed_res) == 0: 
-        sed_res = seds_res[seds_res['Source'].apply(
-            lambda x: x[:-1]) == df.iloc[n-1]['Source'][:-1]]
-        sed_all = seds_all[seds_all['Source'].apply(
-            lambda x: x[:-1]) == df.iloc[n-1]['Source'][:-1]]
-        
-    # Data both lower and higher LOW_HIGH_BORDER (but lower HIGH_BOUND) 
-    # should be present  
-    mask_low = sed_res['log_nu'] < LOW_HIGH_BORDER
-    mask_high = ((sed_res['log_nu']>LOW_HIGH_BORDER) 
-                 & (sed_res['log_nu']<high_bound))
-    if (len(sed_res[mask_low])<1) or (len(sed_res[mask_high])<1):
-        return np.NaN
-    
-    # Polynomial fit
-    sed_pol = sed_res[sed_res['log_nu'] < high_bound]
-    sed_pol = sed_pol.groupby('log_nu')['log_nufnu'].mean().reset_index()
     nu_max, f_max = np.NaN, np.NaN
-    while ((sed_pol.shape[0] > 3) 
-           and (sed_pol[(sed_pol['log_nu']>LOW_HIGH_BORDER)
-                        & (sed_pol['log_nu']<high_bound)].shape[0] > 0)):
+    while sed_pol.shape[0] > 3:
         x0 = np.ones(sed_pol.shape[0])
         x1 = np.array(sed_pol['log_nu'])
         x2 = np.array(sed_pol['log_nu']**2)
@@ -68,21 +37,81 @@ def fit_poly(df, n, seds_res, seds_all, high_bound=15.5):
             break 
                                
         # otherwise remove one point at right
-        sed_pol = sed_pol[:-1]  
+        sed_pol = sed_pol[:-1]
+        
+    return nu_max, f_max, w
+
+
+def fit2deg(sed_pol, high_bound):
+    "Fitting a 2nd degree polynomial"
+    def f(x):
+        return -(w[0] + w[1]*x + w[2]*x**2)
+    
+    nu_max, f_max = np.NaN, np.NaN
+    while sed_pol.shape[0] > 2:
+        x0 = np.ones(sed_pol.shape[0])
+        x1 = np.array(sed_pol['log_nu'])
+        x2 = np.array(sed_pol['log_nu']**2)
+        X = np.column_stack((x0, x1, x2))
+        y = np.array(sed_pol['log_nufnu'])
+        w = np.linalg.inv(X.T@X) @ X.T @ y            
+            
+        # if the polynomial falls at right, then exit
+        if (w[1] + 2*w[2]*high_bound) < 0: 
+            nu_max = -w[1] / (2*w[2])  
+            f_max = -f(nu_max)
+            break 
+                               
+        # otherwise remove one point at right
+        sed_pol = sed_pol[:-1]
+        
+    return nu_max, f_max, w
+
+
+def fit_poly(df, n, seds_res, seds_all, high_bound, polydeg):
+    """Fitting a polynomial to an object's SED
+       n is the row number in the table (df)
+       
+       The function prepares the data and calls fit2deg or fit3deg 
+    """
+    source = df.iloc[n-1]['BZCAT5 Source name']
+    goodbad = df.iloc[n-1]['Correct']
+    sed_res = seds_res[seds_res['Source'] == df.iloc[n-1]['Source']]
+    sed_all = seds_all[seds_all['Source'] == df.iloc[n-1]['Source']]
+    
+    # Sometimes coordinates don't match in Dec arcseconds
+    if len(sed_res) == 0: 
+        sed_res = seds_res[seds_res['Source'].apply(
+            lambda x: x[:-1]) == df.iloc[n-1]['Source'][:-1]]
+        sed_all = seds_all[seds_all['Source'].apply(
+            lambda x: x[:-1]) == df.iloc[n-1]['Source'][:-1]]
+       
+    # Polynomial fit
+    sed_pol = sed_res[sed_res['log_nu'] < high_bound]
+    sed_pol = sed_pol.groupby('log_nu')['log_nufnu'].mean().reset_index()
+    if polydeg == 3:  
+        nu_max, f_max, w = fit3deg(sed_pol, high_bound)
+    else:             
+        nu_max, f_max, w = fit2deg(sed_pol, high_bound)
     
     df.loc[n-1, 'Synch_max'] = nu_max
     df.loc[n-1, 'High_bound'] = high_bound
+    df.loc[n-1, 'Poly_degree'] = polydeg
     df.to_csv(file, index=False)   
     
     return source, nu_max, f_max, w, sed_res, sed_all, goodbad
 
 
-def plot_sed(ax, source, nu_max, f_max, w, sed_res, sed_all, goodbad, n, high_bound=15.5):
+def plot_sed(ax, source, nu_max, f_max, w, sed_res, sed_all, 
+             goodbad, n, high_bound, polydeg):
     "Plotting a SED with a fitted polynomial"
     ax[0].clear()
     ax[1].clear()
     x = np.linspace(7.5, high_bound, 50)
-    y = w[0] + w[1]*x + w[2]*x**2 +w[3]*x**3
+    if polydeg == 3:
+        y = w[0] + w[1]*x + w[2]*x**2 +w[3]*x**3
+    else:
+        y = w[0] + w[1]*x + w[2]*x**2
     ax[0].errorbar(
         sed_res['log_nu'], sed_res['log_nufnu'], yerr=sed_res['log_err'], fmt='.')
     ax[0].plot(x, y)
@@ -99,16 +128,15 @@ def plot_sed(ax, source, nu_max, f_max, w, sed_res, sed_all, goodbad, n, high_bo
         ylabel='$\\log_{10} \\nu F_{\\nu}$, [erg cm$^{-2}$ s$^{-1}$]')
     ax[1].set(xlabel='$\\log_{10} \\nu$, [Hz]')
     fig.canvas.draw()
-    return
 
 
 def add_n(val):
+    "Add/subtract the row number and check if it's within the range"
     global n
     n += val
     if n<1 or n>3561:
         n -= val
         warnings.warn('Object No. is out of range, must be [1:3561]')
-    return
 
 
 def on_press(event):
@@ -118,33 +146,58 @@ def on_press(event):
     sys.stdout.flush()
     if event.key == 'enter' or event.key == ' ':
         add_n(1)
-        update(hb_slider.val)
+        update(True)
     elif event.key == 'backspace':
         add_n(-1)
-        update(hb_slider.val)
+        update(True)
     elif event.key == 'b':
         df.loc[n-1, 'Correct'] = False
         df.to_csv(file, index=False)
-        update(hb_slider.val)
+        update(True)
     elif event.key == 'g':
         df.loc[n-1, 'Correct'] = True
         df.to_csv(file, index=False)
-        update(hb_slider.val)
+        update(True)
     elif event.key == 'c':
         df.loc[n-1, 'Correct'] = np.NaN
         df.to_csv(file, index=False)
-        update(hb_slider.val)
+        update(True)
+
+
+def get_polydeg(label):
+    "Get the polynomial degree from the button labels"
+    poly_dict = {'2nd degree': 2, '3rd degree': 3}
+    return poly_dict[label]
+
+
+def set_true_values():
+    "Setting saved (true) values in the case an object is marked as 'good' "
+    slider_val = df.iloc[n-1]['High_bound']
+    hb_slider.eventson = False
+    hb_slider.set_val(slider_val)
+    hb_slider.eventson = True
+    
+    position_dict = {2:0, 3:1}
+    polydeg = int(df.iloc[n-1]['Poly_degree'])
+    button.eventson = False
+    button.set_active(position_dict[polydeg])
+    button.eventson = True
+    
+    return slider_val, polydeg
 
 
 def update(val):
+    "Response if the slider or button have been changed by the user"
     if df.iloc[n-1]['Correct'] is True:
-        hb_slider.eventson = False
-        hb_slider.set_val(df.iloc[n-1]['High_bound'])
-        hb_slider.eventson = True
-    params = fit_poly(df, n, seds_res, seds_all, hb_slider.val)
-    plot_sed(ax, *params, n, hb_slider.val)
+        slider_val, polydeg = set_true_values()        
+    else:
+        slider_val = hb_slider.val
+        polydeg = get_polydeg(button.value_selected)
+    params = fit_poly(df, n, seds_res, seds_all, slider_val, polydeg)
+    plot_sed(ax, *params, n, hb_slider.val, polydeg)
 
-
+   
+# MAIN PROGRAM
 # Data readout
 if len(sys.argv)!=2 and len(sys.argv)!=3:
     print('\nUsage:\nsed_viewer.py file.csv row_number\n')
@@ -161,6 +214,7 @@ df = pd.read_csv(file)
 df['Synch_max'] = df.get('Synch_max', np.NaN)
 df['Correct'] = df.get('Correct', np.NaN)
 df['High_bound'] = df.get('High_bound', np.NaN)
+df['Poly_degree'] = df.get('Poly_degree', np.NaN)
 seds_res = pd.read_csv(FILE_SEDS_RES)
 seds_all = pd.read_csv(FILE_SEDS_ALL)
 
@@ -200,10 +254,10 @@ plt.rc('axes', titlesize=16)
 plt.rc('axes', labelsize=14)
 fig, ax = plt.subplots(1, 2, figsize=(16, 5))
 
-# adjust the main plot to make room for the sliders
-fig.subplots_adjust(bottom=0.25)
+# adjust the main plot to make room for the slider and button
+fig.subplots_adjust(bottom=0.3)
 
-# Make a horizontal slider to control the frequency.
+# Make a horizontal slider to control the right frequency bound
 axfreq = fig.add_axes([0.2, 0.1, 0.25, 0.03])
 hb_slider = Slider(
     ax=axfreq,
@@ -214,6 +268,14 @@ hb_slider = Slider(
 )
 hb_slider.on_changed(update)
 
+# Radio button: 2nd or 3rd degree polynomial
+#axcolor = 'lightgoldenrodyellow'
+rax = fig.add_axes([0.5, 0.07, 0.08, 0.1])#, facecolor=axcolor)
+button = RadioButtons(rax, ('2nd degree', '3rd degree'), active=1)
+button.on_clicked(update)
+
+# Connect the figure to keyboard hotkeys
 fig.canvas.mpl_connect('key_press_event', on_press)
-update(hb_slider.val)
+update(True)
+
 plt.show()
